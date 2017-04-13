552e0b9990514bebac5468a88806cccc9294cb25
Anuraag93 <anuraagbanger@gmail.com>
2017-04-13 09:05:54 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,19 @@
+class program1
+{
+	pubic static void main(String[] args)
+	{
+		System.out.println("prg starts...");
+		int a;
+		a=50;
+		System.out.println("a");
+		float b=5.6f;
+		System.out.println("f");
+		long c = 52l;
+		long d = 45;
+		char ch = 'a';
+		System.out.println("c");
+		System.out.println("d");
+		System.out.println("ch");
+		System.out.println("prg ends...");
+	}
+}
---
+++ program1.java
@@ -0,0 +1,15 @@
+class program1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("prg starts...");
+		System.out.println("Hello World!");
+		System.out.println("Hello "+"World");
+		System.out.println("Hello "+"World "+10);
+		System.out.println(10+" Hello "+"World");
+		System.out.println("Hello "+"World"+10+10);
+		System.out.println(10+10"Hello "+"World");
+		System.out.println(10+10+"Hello "+"World"+10+10);
+		System.out.println("prg ends...");
+	}
+}
